d37ef06a2dc3a44bd6da627163ac8d7d530750b2
John <flnc85@hotmail.de>
2021-11-10 19:41:50 +0100

    erster Commit Test111

---
+++ src/test111/Test111.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test111;
+
+/**
+ *
+ * @author flnc8
+ */
+public class Test111 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ich bin ein Test");
+    }
+    
+}
